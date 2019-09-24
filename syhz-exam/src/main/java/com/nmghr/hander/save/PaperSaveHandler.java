afd93ce6bf55af2f1d01e782fd19fd2b4a34d813
liubo <Lb@123>
2019-09-24 21:06:28 +0800

    考试卷子维护 ，试题hanlder， 考试记录等

--- syhz-exam/src/main/java/com/nmghr/hander/save/PaperSaveHandler.java
+++ syhz-exam/src/main/java/com/nmghr/hander/save/PaperSaveHandler.java
@@ -2,7 +2,6 @@ package com.nmghr.hander.save;
 
 import com.alibaba.fastjson.JSONObject;
 import com.nmghr.basic.common.Constant;
-import com.nmghr.basic.common.exception.GlobalErrorEnum;
 import com.nmghr.basic.common.exception.GlobalErrorException;
 import com.nmghr.basic.core.common.LocalThreadStorage;
 import com.nmghr.basic.core.service.IBaseService;
@@ -140,7 +139,7 @@ public class PaperSaveHandler extends AbstractSaveHandler {
   private void packageParams(Map<String, Object> bean, Map<String, Object> params, String key, int type) {
     JSONObject remark = (JSONObject) params.get("remark");
     if (bean != null) {
-      remark.put(key, bean.get("desc") + "#" + bean.get("sort") + "#" + bean.get("value"));
+      remark.put(key, bean.get("desc") + ExamConstant.DESCFLAG + bean.get("sort") + ExamConstant.DESCFLAG + bean.get("value"));
       params.put("remark", remark);
       List<Map<String, Object>> list = (List<Map<String, Object>>) params.get("questionList");
       List<Map<String, Object>> datas = (List<Map<String, Object>>) bean.get("data");
