7ef13095fe10c7e2ceb4c14a5f3d2bcb186fea68
heijiantao <heijiantao@qq.com>
2019-12-20 19:04:53 +0800

    线索管理：”采集类型”改为“线索来源”，导出线索表格长度修改，新增线索来源字典

--- syhz-spring-cloud-starter/src/main/java/com/nmghr/hander/query/PersonMessageQueryHandler.java
+++ syhz-spring-cloud-starter/src/main/java/com/nmghr/hander/query/PersonMessageQueryHandler.java
@@ -65,6 +65,7 @@ public class PersonMessageQueryHandler extends AbstractQueryHandler {
 		List<Map<String, Object>> hbzt = new ArrayList<Map<String, Object>>();
 		List<Map<String, Object>> hbyy = new ArrayList<Map<String, Object>>();
 		List<Map<String, Object>> cfhbcz = new ArrayList<Map<String, Object>>();
+		List<Map<String, Object>> xsly = new ArrayList<Map<String, Object>>();
 
     for (int i = 0; i < list.size(); i++) {
       if ("mz".equals(String.valueOf(list.get(i).get("codeLx")))) {
@@ -147,6 +148,8 @@ public class PersonMessageQueryHandler extends AbstractQueryHandler {
 				hbyy.add(list.get(i));
 			} else if ("cfhbcz".equals(String.valueOf(list.get(i).get("codeLx")))) {
 				cfhbcz.add(list.get(i));
+			} else if ("xsly".equals(String.valueOf(list.get(i).get("codeLx")))) {
+				xsly.add(list.get(i));
       }
     }
     Map<String, Object> map = new HashMap<String, Object>();
@@ -187,9 +190,10 @@ public class PersonMessageQueryHandler extends AbstractQueryHandler {
     map.put("fllb", fllb);
     map.put("qbxsffzt", qbxsffzt);
     map.put("flowStatus", flowStatus);
-		map.put("hbzt", hbzt);
-		map.put("hbyy", hbyy);
-		map.put("cfhbcz", cfhbcz);
+    map.put("hbzt", hbzt);
+    map.put("hbyy", hbyy);
+    map.put("cfhbcz", cfhbcz);
+		map.put("xsly", xsly);
 
     return map;
 
