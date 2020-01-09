6608a74db6e9d6fb600bcdd8ce71676764d2d933
wrx <435102806@qq.com>
2020-01-09 16:11:58 +0800

    提交采集线索统计分析折线、饼状图后端接口

---
+++ syhz-spring-cloud-starter/src/main/java/com/nmghr/controller/information/ClueStatisticsController.java
@@ -0,0 +1,116 @@
+/**
+ * Created by wrx on 2020/1/7
+ * <p/>
+ * Copyright (c) 2015-2015
+ * Apache License
+ * Version 2.0, January 2004
+ * http://www.apache.org/licenses/
+ */
+package com.nmghr.controller.information;
+
+import com.nmghr.basic.common.exception.GlobalErrorException;
+import com.nmghr.basic.core.service.IBaseService;
+import com.nmghr.basic.core.service.handler.IQueryHandler;
+import com.nmghr.basic.core.util.SpringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("clueStatistics")
+public class ClueStatisticsController {
+
+    @Autowired
+    @Qualifier("baseService")
+    private IBaseService baseService;
+
+    /**
+     * 线索分类统计——按分类
+     */
+    @GetMapping("/byClassify")
+    public Object statisticsByClassify(@RequestParam Map<String, Object> requestParam) throws Exception{
+        if(requestParam.get("collectionDateStart") == null){
+            throw new GlobalErrorException("998001", "采集开始时间不能为空");
+        }
+        if(requestParam.get("collectionDateEnd") == null){
+            throw new GlobalErrorException("998001", "采集结束时间不能为空");
+        }
+        IQueryHandler queryHandler = SpringUtils.getBean("clueStatisticsByClassifyHandler", IQueryHandler.class);
+        return queryHandler.list(requestParam);
+    }
+
+    /**
+     * 线索分类统计——按分类和时间
+     */
+    @GetMapping("/byClassifyAndDate")
+    public Object statisticsByClassifyAndDate(@RequestParam Map<String, Object> requestParam) throws Exception{
+        if(requestParam.get("collectionDateStart") == null){
+            throw new GlobalErrorException("998001", "采集开始时间不能为空");
+        }
+        if(requestParam.get("collectionDateEnd") == null){
+            throw new GlobalErrorException("998001", "采集结束时间不能为空");
+        }
+        if(requestParam.get("timeDimensionType") == null){
+            throw new GlobalErrorException("998001", "时间维度类型不能为空");
+        }
+        IQueryHandler queryHandler = SpringUtils.getBean("clueStatisticsByClassifyAndDateHandler", IQueryHandler.class);
+        return queryHandler.list(requestParam);
+    }
+
+
+
+    /**
+     * 线索分类统计——按分类和时间
+     */
+    @GetMapping("/bySourceAndDate")
+    public Object statisticsBySourceAndDate(@RequestParam Map<String, Object> requestParam) throws Exception{
+        if(requestParam.get("collectionDateStart") == null){
+            throw new GlobalErrorException("998001", "采集开始时间不能为空");
+        }
+        if(requestParam.get("collectionDateEnd") == null){
+            throw new GlobalErrorException("998001", "采集结束时间不能为空");
+        }
+        if(requestParam.get("timeDimensionType") == null){
+            throw new GlobalErrorException("998001", "时间维度类型不能为空");
+        }
+        IQueryHandler queryHandler = SpringUtils.getBean("clueStatisticsBySourceAndDateHandler", IQueryHandler.class);
+        return queryHandler.list(requestParam);
+    }
+
+    /**
+     * 线索分类统计——按分类
+     */
+    @GetMapping("/bySource")
+    public Object statisticsBySource(@RequestParam Map<String, Object> requestParam) throws Exception{
+        if(requestParam.get("collectionDateStart") == null){
+            throw new GlobalErrorException("998001", "采集开始时间不能为空");
+        }
+        if(requestParam.get("collectionDateEnd") == null){
+            throw new GlobalErrorException("998001", "采集结束时间不能为空");
+        }
+        IQueryHandler queryHandler = SpringUtils.getBean("clueStatisticsBySourceHandler", IQueryHandler.class);
+        return queryHandler.list(requestParam);
+    }
+
+    /**
+     * 线索区域统计——多区域
+     */
+//    @GetMapping("/byArea")
+//    public Object statisticsByArea(@RequestParam Map<String, Object> requestParam) throws Exception{
+//        if(requestParam.get("collectionDateStart") == null){
+//            throw new GlobalErrorException("998001", "采集开始时间不能为空");
+//        }
+//        if(requestParam.get("collectionDateEnd") == null){
+//            throw new GlobalErrorException("998001", "采集结束时间不能为空");
+//        }
+//        if(requestParam.get("area") == null){
+//            throw new GlobalErrorException("998001", "区域集合不能为空");
+//        }
+//        IQueryHandler queryHandler = SpringUtils.getBean("clueStatisticsByAreaHandler", IQueryHandler.class);
+//        return queryHandler.list(requestParam);
+//    }
+
+}
